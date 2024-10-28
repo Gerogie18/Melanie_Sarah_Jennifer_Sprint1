d16ac69913e5942807889bb3a38da78c2b278e84
Sarah Kristian Perry <sarah.perry@keyin.com>
2024-10-28 17:10:04 +0000

    fix anagrams to display one at a time but solution validator isn't working.

--- scripts/anagrams.js
+++ scripts/anagrams.js
@@ -1,25 +1,25 @@
 console.log("JS loaded")
 
-// get data 
-window.addEventListener("DOMContentLoaded", function () {
-  fetch("../data/anagrams.json")
-    .then(response => response.json())
-    .then(data => {
-      totalCount = 0;
-      data.forEach((item, index) => {
-        new Anagram(item.scrambled_word, item.longest_soln, item.solutions, item.rating, index); // Create a new instance for each trivia question
-        console.log("anagrams JSON file read");
-        totalCount +=1;
-      });
-    });
-});
+// SET UP CLASSES
 
+// to store user data 
+//****** (incomplete)****** 
+class userData {
+  constructor(id, anaScore, madlibValues, triviaScore){
+    this.id = id;
+    this.anaScore = anaScore;
+    this.madlibValues = madlibValues;
+    this.triviaScore = triviaScore;
+  }
+}
+// to create anagrams
+//******** Probably this class handles too much? Consider refactoring (esp. CreateSolnFields)******
 class Anagram{
-  constructor(scrambled_word, longest_soln, solutions, rating, index){
+  constructor(scrambled_word, longest_soln, solutions, challenge_rating, index){
     this.scrambled_word = scrambled_word;
     this.longest_soln = longest_soln;
     this.solutions = solutions;
-    this.rating = rating;
+    this.challenge_rating = challenge_rating;
     this.index = index;
 
     // Initialize the creation of DOM elements
@@ -32,8 +32,8 @@ class Anagram{
   // Create question div and display question
   createQuestionDiv() {
     let div = document.createElement("div");
-    div.id = `anaQ${this.index}_${this.rating}`;
-    div.className = "container pt-4 text-center";
+    div.id = `anaQ${this.index}_${this.challenge_rating}`;
+    div.className = "container pt-4 text-center hide";
     let title = this.scrambled_word.toLowerCase();
     div.innerHTML = `<h2>${title}</h2>`;
     document.querySelector("#anaQuestionRow").appendChild(div);
@@ -43,7 +43,7 @@ class Anagram{
     let div = document.createElement("div");
     div.id = `anaQ${this.index}`;
     div.className = "container";
-    document.querySelector(`#anaQ${this.index}_${this.rating}`).appendChild(div);
+    document.querySelector(`#anaQ${this.index}_${this.challenge_rating}`).appendChild(div);
     console.log("Anagram solutions div created")
   };
   
@@ -100,14 +100,32 @@ class Anagram{
 
 
 
-class userData {
-  constructor(id, anaScore, madlibValues, triviaScore){
-    this.id = id;
-    this.anaScore = anaScore;
-    this.madlibValues = madlibValues;
-    this.triviaScore = triviaScore;
-  }
-}
+
+// GET DATA
+
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  fetch("../data/anagrams.json")
+    .then(response => response.json())
+    .then(data => {
+      totalCount = 0;
+      data.forEach((item, index) => {
+        new Anagram(item.scrambled_word, item.longest_soln, item.solutions, item.rating, index); // Create a new instance for each trivia question
+        console.log("anagrams JSON file read");
+        totalCount +=1;
+      });
+    });
+});
+
+
+
+
+// PROCESS ANAGRAM DATA
+
+
+
+
 
 
 // Create ID 
@@ -119,6 +137,7 @@ const randomID = generateRandomID();
 console.log(randomID); // Example output: "a1Bc"
 
 
+
 // form validation
 document.querySelectorAll("[id^='ch']").forEach((input) => input.addEventListener("input", function(e) {
     this.value = this.value.replace(/[^A-Za-z]/g, ''); // Remove non-alphabetic characters
@@ -126,19 +145,18 @@ document.querySelectorAll("[id^='ch']").forEach((input) => input.addEventListene
 })
 );
 
-
 // process form
-let form = document.querySelector("#anaForm");
-form.addEventListener("submit", function (event) {
-  // collect information from all the text fields...
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const data = {};
-  formData.forEach((value, key) => data[key] = value);
-  console.log(data);
-  let word = createWord(data);
-  console.log(word)
-});
+// let form = document.querySelector("#anaForm");
+// form.addEventListener("submit", function (event) {
+//   // collect information from all the text fields...
+//   event.preventDefault();
+//   const formData = new FormData(event.target);
+//   const data = {};
+//   formData.forEach((value, key) => data[key] = value);
+//   console.log(data);
+//   let word = createWord(data);
+//   console.log(word)
+// });
 
 function createWord(letters){
   let word = ''
@@ -148,4 +166,78 @@ function createWord(letters){
   console.log(word);
   return word;
 }
+
+
+// HANDLE BUTTON ACTIONS
+
+function enableButton(buttonID, eventHandler){
+  // button.disabled = false; // enable the button
+  document.getElementById(buttonID).addEventListener('click', eventHandler);
+  console.log("button enabled");
+}
+
+
+function disableButton(button, eventHandler) {
+  // button.disabled = true; // Disable the button
+  button.removeEventListener("click", eventHandler); // Remove event listener
+  console.log("button disabled");
+}
+
+
+// Play Button
+function handlePlayClick() {
+  console.log('anagrams play button clicked');
+
+  // Show first Question and Next button
+  let showDiv = document.querySelector("[id^='anaQ0_']");
+  showDiv.classList.remove('hide');
+
+  let nextButton = document.getElementById('anaNextButton');
+  nextButton.classList.remove('hide');
+
+  //Hide demon and Play button
+  let hideDiv = document.getElementById('anaDemo');
+  hideDiv.classList.add('hide');
+
+  let button = document.getElementById('anaPlayButton');
+  disableButton(button, handlePlayClick);
+  button.classList.add('hide');
+}
+
+
+// Next Button
+let currCount = 0;
+function handleNextClick() {
+  console.log(currCount, "of", totalCount);
+  if (currCount < (totalCount-1)){
+    
+    //Show nextDiv
+    let newCount = (currCount + 1);
+    let showDiv = document.querySelector(`[id^='anaQ${newCount}_']`);
+    showDiv.classList.remove('hide');
+
+    //Hide curr Div
+    let hideDiv = document.querySelector(`[id^='anaQ${currCount}_']`);
+    hideDiv.classList.add('hide');
+    console.log(currCount, newCount, totalCount);
+
+    currCount ++;
+
+    //Disable Button
+      if (newCount >= (totalCount-1)){
+      console.log("reached last question");
+      let nextButton = document.getElementById('anaNextButton');
+      nextButton.classList.remove('btn-primary');
+      disableButton(nextButton, handleNextClick)}
+    }
+}
+
+
+// play Anagrams
+document.getElementById('anaPlayButton').addEventListener('click', handlePlayClick);
+document.getElementById('anaNextButton').addEventListener('click', handleNextClick);
+
+//run JS
+
+// Add event listener to the "play/next" button
 
