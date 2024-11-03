737be2d19504364d21187d9b3ab3d53a47df8a1c
Sarah Kristian Perry <sarah.perry@keyin.com>
2024-11-03 11:53:13 +0000

    fix links in index; added comments in ghost and home

--- scripts/ghost.js
+++ scripts/ghost.js
@@ -1,3 +1,9 @@
+// Name: SD 12 group: Melanie
+// Date: Nov 1, 2024
+// Description: Add ghost to chase cursor
+
+
+
 $(document).ready(function(){
   $(document).mousemove(function(e){
     $('.ghost').css('left',e.pageX+"px");
